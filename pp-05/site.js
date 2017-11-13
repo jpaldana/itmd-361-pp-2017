79423d0bdb8711d1b0a623c95f0ddcffe6615c08
jpaldana <aldanajpg@gmail.com>
2017-11-13 14:09:13 -0600

    Move ringDoorbell() inside body of anonymous function

--- pp-05/site.js
+++ pp-05/site.js
@@ -1,12 +1,12 @@
 // Release the dollar-sign from jQuery's control
 $.noConflict();
 
-function ringDoorbell() {
-  var doorbell = new Audio('media/doorbell.mp3');
-  doorbell.play();
-}
-
 (function($) {
+  function ringDoorbell() {
+    var doorbell = new Audio('media/doorbell.mp3');
+    doorbell.play();
+  }
+  
   /* remove audio element for js-supported browsers */
   $("audio").remove();
 
