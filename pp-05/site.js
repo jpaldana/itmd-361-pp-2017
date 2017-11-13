cda8d5dd6ba42290fe5e6fb43ca33a6216d36e88
jpaldana <aldanajpg@gmail.com>
2017-11-13 11:54:41 -0600

    Remove audio element for js-supported browsers

--- pp-05/site.js
+++ pp-05/site.js
@@ -2,3 +2,8 @@ function ringDoorbell() {
   var doorbell = new Audio('media/doorbell.mp3');
   doorbell.play();
 }
+
+$(function() {
+  /* remove audio element for js-supported browsers */
+  $("audio").remove();
+});
